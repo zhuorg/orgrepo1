d7e5744ef3e23d71d363b38b6f3324ba89713abc
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-05-13 14:29:06 +0800

    Create fail-script.ps1

---
+++ .github/bin/fail-script.ps1
@@ -0,0 +1,8 @@
+try {
+          throw (new-object "Exception" @('foo'))
+     } 
+catch {
+          write-host "caught failure: $_"
+}
+throw "Failed"
+Write-Verbose -Message "Searching the Application Event Log."

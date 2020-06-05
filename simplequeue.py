6a6afdc10f843b67a488878111e6cb151be1e8d2
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-06-05 17:15:22 +0800

    Update simplequeue.py

--- simplequeue.py
+++ simplequeue.py
@@ -1,3 +1,4 @@
+import pika
 import socket
 import time
 import os
@@ -9,31 +10,33 @@ def receiveMessage(channel, method, properties, body):
 
 print('**** Running simplequeue.py ****')
 
-isreachable = False
-pingCounter = 0
-print('**** first we see if we can reach the server')
-while isreachable is False and pingCounter < 10:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        # s.connect(('0.0.0.0',15672))
-        s.connect(('0.0.0.0', 5672))
-        isreachable = True
-        print('[x] found the server')
-    except socket.error as e:
-        time.sleep(2)
-        pingCounter += 1
-    s.close()
-
-if isreachable is False:
-    print('[!] Could not get a socketfor <host>:5672')
-else:
-    print('[x] socket to host worked')
-
-
-# rabbitmq_port = os.environ['RABBITMQ_PORT']
 amqp_url = os.environ['AMQP_URL']
-# print(rabbitmq_port)
 print(amqp_url)
 
 urlConnectionParameters = pika.URLParameters(amqp_url)
-connection = pika.BlockingConnection(urlConnectionParameters)
+
+# # ### choose which parameter set
+parameters = urlConnectionParameters
+   
+try:
+    print('[x] about to build a blocking connection')
+    connection = pika.BlockingConnection(parameters)
+    print('[x] built a connection')
+    channel = connection.channel()
+    channel.queue_declare(queue='first_queue')
+
+    channel.basic_publish(exchange='', routing_key='first_queue', body='Hi Queue')
+    print('[x] send a first message to the queue')
+
+
+    time.sleep(5)
+
+    channel.basic_consume(queue='first_queue', auto_ack=True, on_message_callback=receiveMessage)
+
+    print('[x] Waiting for messages')
+
+    channel.start_consuming()
+    connection.close()
+except:
+    print("[!] We could not get a connection")
+    pass

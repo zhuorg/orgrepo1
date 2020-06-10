bced847cad21edc367b57e344aefabe6c0ea43e9
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-06-10 16:28:42 +0800

    Update simplequeue.py

--- simplequeue.py
+++ simplequeue.py
@@ -13,6 +13,29 @@ def receiveMessage(channel, method, properties, body):
 
 print('**** Running simplequeue.py ****')
 
+
+isreachable = False
+pingCounter = 0
+print('**** first we see if we can reach the server')
+while isreachable is False and pingCounter < 10:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # s.connect(('0.0.0.0',5672))
+        s.connect(('localhost', os.environ['RABBITMQ_PORT']))
+        isreachable = True
+        print('[x] found the server')
+    except socket.error as e:
+        time.sleep(2)
+        pingCounter += 1
+    s.close()
+
+if isreachable is False:
+    print('[!] Could not get a socketfor <host>:port')
+else:
+    print('[x] socket to host worked')
+
+
+
 amqp_url = os.environ['AMQP_URL']
 print(amqp_url)
 

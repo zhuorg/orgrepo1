4b093265d9a9e521035fefa163b407a8fe954a86
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-06-26 13:52:29 +0800

    Create locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,20 @@
+
+from locust import HttpUser, TaskSet, task, between
+
+
+
+class UserTasks(TaskSet):
+
+	@task(1)
+
+	def index(self):
+
+		self.client.get("/")
+
+
+
+class WebsiteUser(HttpUser):
+
+	tasks = [UserTasks]
+
+	wait_time = between(1, 5)
